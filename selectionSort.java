c75d7250932b589a134256bbe48fc8f99d2cf8a3
himsharma1017 <126320820+himsharma1017@users.noreply.github.com>
2023-04-16 19:51:26 +0530

    Add files via upload

---
+++ selectionSort.java
@@ -0,0 +1,26 @@
+public class selectionSort {
+    
+    public static void selection(int arr[]){
+        for(int i = 0; i<arr.length-1; i++){
+            int minIndex = i;
+
+            for(int j = i+1; j<arr.length; j++)
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+               
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+public static void main(String[] args) {
+    int arr[] = {20,10,30,5,7};
+    selection(arr);
+    // System.out.println(Arrays.toString(arr));
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+ " ");
+    }
+} 
+}
+
